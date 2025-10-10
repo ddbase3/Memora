a4c8bb6c9fbc55f643943b14852210f49bf1fe0d
Daniel Dahme <info@base3.de>
2025-10-10 19:46:08 +0200

    implementation

--- src/Service/MemoraEntityDataService.php
+++ src/Service/MemoraEntityDataService.php
@@ -12,49 +12,95 @@ class MemoraEntityDataService implements IEntityDataService {
 	// Implementation of IEntityDataService
 
 	public function getEntries(array $options = []): array {
-
 		$query = [
 			"type" => "select",
 			"fields" => [
 				[
-					"element" => [
-						"type" => "fld",
-						"table" => "base3system_sysentry",
-						"field" => "id"
-					],
+					"element" => [ "type" => "fld", "table" => "base3system_sysentry", "field" => "id" ],
 					"alias" => "id"
-				], [
-					"element" => [
-						"type" => "fld",
-						"table" => "base3system_systype",
-						"field" => "alias"
-					],
-					"alias" => "alias"
-				], [
-					"element" => [
-						"type" => "fld",
-						"table" => "base3system_sysname",
-						"field" => "name"
-					],
+				],
+				[
+					"element" => [ "type" => "fld", "table" => "base3system_sysname", "field" => "name" ],
 					"alias" => "name"
+				],
+				[
+					"element" => [ "type" => "fld", "table" => "base3system_systype", "field" => "alias" ],
+					"alias" => "type_alias"
 				]
 			],
 			"table" => "base3system_sysentry",
-			"order_by" => [
-				[
-					"element" => [
-						"type" => "fld",
-						"table" => "base3system_sysentry",
-						"field" => "id"
-					],
-					"direction" => "desc"
-				]
-			],
-			"limit" => 10
+			"where" => null,
+			"order_by" => [],
 		];
 
-		$result = $this->dataqueryservice->executeQuery($query);
+		// --- dynamic WHERE conditions ---
+		$where = [];
 
+		// Filter by entry ID(s)
+		if (!empty($options["entry"])) {
+			$ids = is_array($options["entry"]) ? $options["entry"] : [$options["entry"]];
+			$where[] = [
+				"type" => "op",
+				"operator" => "IN",
+				"params" => [
+					[ "type" => "fld", "table" => "base3system_sysentry", "field" => "id" ],
+					$ids
+				]
+			];
+		}
+
+		// Filter by type
+		if (!empty($options["type"])) {
+			$where[] = [
+				"type" => "op",
+				"operator" => "=",
+				"params" => [
+					[ "type" => "fld", "table" => "base3system_systype", "field" => "alias", "variant" => "required" ],
+					$options["type"]
+				]
+			];
+		}
+
+		// Filter by tag
+		if (!empty($options["tag"])) {
+			$tags = is_array($options["tag"]) ? $options["tag"] : [$options["tag"]];
+			$where[] = [
+				"type" => "op",
+				"operator" => "IN",
+				"params" => [
+					[ "type" => "fld", "table" => "base3system_systag", "field" => "tag", "variant" => "optional" ],
+					$tags
+				]
+			];
+		}
+
+		// Combine conditions
+		if (count($where) === 1) {
+			$query["where"] = $where[0];
+		} elseif (count($where) > 1) {
+			$query["where"] = [
+				"type" => "op",
+				"operator" => "AND",
+				"params" => $where
+			];
+		}
+
+		// --- ORDER ---
+		$query["order_by"][] = [
+			"element" => [ "type" => "fld", "table" => "base3system_sysentry", "field" => "id" ],
+			"direction" => $options["orderdir"] ?? "DESC"
+		];
+
+		// --- LIMIT ---
+		if (!empty($options["limitcount"])) {
+			$query["limit"] = (int)$options["limitcount"];
+			if (!empty($options["limitoffset"])) {
+				$query["offset"] = (int)$options["limitoffset"];
+			}
+		}
+
+		// --- Execute ---
+		$result = $this->dataqueryservice->executeQuery($query);
 		return $result->rows;
 	}
 
