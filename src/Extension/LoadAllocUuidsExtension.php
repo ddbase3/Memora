90bf5658464866ba95ec77074d9e4464555124c3
Daniel Dahme <info@base3.de>
2026-06-14 19:54:08 +0200

    add alloc uuid loading extension

--- src/Extension/LoadAllocUuidsExtension.php
+++ src/Extension/LoadAllocUuidsExtension.php
@@ -7,103 +7,81 @@ use Memora\Api\IMemoraQueryExtension;
 
 class LoadAllocUuidsExtension implements IMemoraQueryExtension, ISortable {
 
-	public function isApplicable(array $options): bool {
-		return !empty($options['loadallocuuids']);
-	}
+        public function isApplicable(array $options): bool {
+                return !empty($options['loadallocuuids']);
+        }
 
-	public function applyToQuery(array $query, array $options): array {
-		$query['leftjoin'][] = [
-			'table' => 'base3system_sysallocview',
-			'alias' => 'loadallocuuids_alloc',
-			'on' => [
-				'type' => 'op',
-				'operator' => '=',
-				'params' => [
-					[
-						'type' => 'fld',
-						'tablealias' => 'loadallocuuids_alloc',
-						'field' => 'entry_id'
-					],
-					[
-						'type' => 'fld',
-						'table' => 'base3system_sysentry',
-						'field' => 'id'
-					]
-				]
-			]
-		];
+        public function applyToQuery(array $query, array $options): array {
+                // Explicitly request the allocation view alias.
+                // This is required so DataHawk can build:
+                // sysentry -> sysallocview AS loadallocuuidalloc -> sysentry AS loadallocuuidpeer
+                $query['fields'][] = [
+                        'element' => [
+                                'type' => 'fn',
+                                'function' => 'GROUP_CONCAT',
+                                'distinct' => true,
+                                'params' => [
+                                        [
+                                                'type' => 'fld',
+                                                'table' => 'base3system_sysallocview',
+                                                'tablealias' => 'loadallocuuidalloc',
+                                                'field' => 'peer_id',
+                                                'variant' => 'optional'
+                                        ]
+                                ]
+                        ],
+                        'alias' => 'allocuuidpeerids'
+                ];
 
-		$query['leftjoin'][] = [
-			'table' => 'base3system_sysentry',
-			'alias' => 'loadallocuuids_peer',
-			'on' => [
-				'type' => 'op',
-				'operator' => '=',
-				'params' => [
-					[
-						'type' => 'fld',
-						'tablealias' => 'loadallocuuids_peer',
-						'field' => 'id'
-					],
-					[
-						'type' => 'fld',
-						'tablealias' => 'loadallocuuids_alloc',
-						'field' => 'peer_id'
-					]
-				]
-			]
-		];
+                $query['fields'][] = [
+                        'element' => [
+                                'type' => 'fn',
+                                'function' => 'GROUP_CONCAT',
+                                'distinct' => true,
+                                'params' => [
+                                        [
+                                                'type' => 'fn',
+                                                'function' => 'HEX',
+                                                'params' => [
+                                                        [
+                                                                'type' => 'fld',
+                                                                'table' => 'base3system_sysentry',
+                                                                'tablealias' => 'loadallocuuidpeer',
+                                                                'field' => 'uuid',
+                                                                'variant' => 'optional'
+                                                        ]
+                                                ]
+                                        ]
+                                ]
+                        ],
+                        'alias' => 'allocuuids'
+                ];
 
-		$query['fields'][] = [
-			'element' => [
-				'type' => 'fn',
-				'function' => 'GROUP_CONCAT',
-				'params' => [
-					[
-						'type' => 'fn',
-						'function' => 'DISTINCT',
-						'params' => [
-							[
-								'type' => 'fn',
-								'function' => 'HEX',
-								'params' => [
-									[
-										'type' => 'fld',
-										'tablealias' => 'loadallocuuids_peer',
-										'field' => 'uuid'
-									]
-								]
-							]
-						]
-					]
-				]
-			],
-			'alias' => 'allocuuids'
-		];
+                return $query;
+        }
 
-		return $query;
-	}
+        public function processResult(array $rows, array $options): array {
+                foreach ($rows as &$row) {
+                        unset($row['allocuuidpeerids']);
 
-	public function processResult(array $rows, array $options): array {
-		foreach ($rows as &$row) {
-			if (!empty($row['allocuuids']) && is_string($row['allocuuids'])) {
-				$uuids = explode(',', $row['allocuuids']);
-				$uuids = array_map('trim', $uuids);
-				$uuids = array_filter($uuids, fn($uuid) => $uuid !== '');
-				$uuids = array_map('strtolower', $uuids);
-				$uuids = array_values(array_unique($uuids));
+                        if (!empty($row['allocuuids']) && is_string($row['allocuuids'])) {
+                                $uuids = explode(',', $row['allocuuids']);
+                                $uuids = array_map('trim', $uuids);
+                                $uuids = array_filter($uuids, static fn(string $uuid): bool => $uuid !== '');
+                                $uuids = array_map('strtolower', $uuids);
+                                $uuids = array_values(array_unique($uuids));
 
-				$row['allocuuids'] = $uuids;
-			} else {
-				$row['allocuuids'] = [];
-			}
-		}
-		unset($row);
+                                $row['allocuuids'] = $uuids;
+                        } else {
+                                $row['allocuuids'] = [];
+                        }
+                }
+                unset($row);
 
-		return $rows;
-	}
+                return $rows;
+        }
 
-	public function getPriority(): int {
-		return 845;
-	}
+        public function getPriority(): int {
+                return 845;
+        }
 }
